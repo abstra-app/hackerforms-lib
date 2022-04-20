e5dddf35b0db4f6a5b83f5ca7eb3871d46783365
Igor Torres <igorctoliveira@gmail.com>
2022-04-20 17:03:42 -0300

    Use classes for input and outputs

--- lib/hackerforms/inputs.py
+++ lib/hackerforms/inputs.py
@@ -1,60 +1,119 @@
+from abc import abstractmethod, ABC
 from typing import List, Union, Dict
 
 from .type_classes import FileResponse
 from .socket import send, receive
 
+class Input(ABC):
+    type: str
 
-def read_text(msg: str) -> Dict:
-    return {
-        'message': msg,
-        'type': 'text-input',
-    }
+    @abstractmethod
+    def json():
+        pass
+
+    def convertAnswer(self, answer):
+        return answer
+    
+class TextInput(Input):
+    type = 'text-input'
+    def __init__(self, message: str):
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.message,
+        }
+
+class DateInput(Input):
+    type = 'date-input'
+    def __init__(self, message: str):
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.message,
+        }
+
+class FileInput(Input):
+    type = 'file-input'
+    def __init__(self, message: str):
+        self.message = message
+
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.message,
+        }
+
+    def convertAnswer(self, answer):
+        return FileResponse(answer)
+
+class MultipleChoiceInput(Input):
+    type = 'multiple-choice-input'
+    def __init__(self, message: str, options: Union[List[str], List[Dict]], multiple: bool = False):
+        self.message = message
+        self.option = options
+        self.multiple = multiple
+
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.message,
+            'options': self.options,
+            'multiple': self.multiple,
+        }
+
+class DropdownInput(Input):
+    type = 'dropdown-input'
+    def __init__(self, message: str, options: Union[List[str], List[Dict]]):
+        self.message = message
+        self.option = options
+
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.message,
+            'options': self.options,
+        }
+
+def read_text(message: str, button_text: str = "Next") -> Dict:
+    return read_form([TextInput(message)], button_text)
 
 
-def read_date(msg: str) -> Dict:
-    return {
-        'message': msg,
-        'type': 'date-input',
-    }
+def read_date(message: str, button_text: str = "Next") -> Dict:
+    return read_form([DateInput(message)], button_text)
 
 
-def read_file(msg: str) -> Dict:
-    return {
-        'message': msg,
-        'type': 'file-input',
-    }
+def read_file(message: str, button_text: str = "Next") -> Dict:
+    return read_form([FileInput(message)], button_text)
 
 
-def read_multiple_choice(msg: str,
+def read_multiple_choice(message: str,
                          options: Union[List[str], List[Dict]],
-                         multiple: bool = False) -> Dict:
-    return {
-        'message': msg,
-        'type': 'multiple-choice-input',
-        'options': options,
-        'multiple': multiple
-    }
+                         multiple: bool = False, 
+                         button_text: str = "Next") -> Dict:
+    
+    return read_form([MultipleChoiceInput(message, options, multiple)], button_text)
 
 
-def read_dropdown(name: str, options: Union[List[str], List[Dict]]) -> Dict:
+def read_dropdown(name: str, options: Union[List[str], List[Dict]], button_text: str = "Next") -> Dict:
     return {
-        'message': name,
         'type': 'dropdown-input',
+        'message': name,
         'options': options
     }
 
-def read_form(fields: List[Dict], button_text: str = "Next"):
+def read_form(inputs: List[Input], button_text: str = "Next"):
     send({
         'type': 'form-input',
-        'fields': fields,
+        'fields': [input.json() for input in inputs],
         'buttonText': button_text
     })
     answers = receive('payload')
-    
-    def convertAnswer(field: Dict, answer: any):
-        if field['type'] == 'file-input':
-            return FileResponse(answer)
-        else:
-            return answer
-    
-    return list(map(convertAnswer, fields, answers))
+
+    if len(inputs) == 1:
+        return inputs[0].convertAnswer(answers[0])
+
+    return [input.convertAnswer(answers[index]) for index, input in enumerate(inputs)]
--- lib/hackerforms/outputs.py
+++ lib/hackerforms/outputs.py
@@ -1,48 +1,97 @@
+from abc import abstractmethod, ABC
 from typing import List, Dict
 from .apis import upload_file
 from .socket import send, receive
 
+class Output(ABC):
+    type: str
 
-def display_text(msg: str) -> None:
-    return {
-        'message': msg,
-        'type': 'text-output',
-    }
+    @abstractmethod
+    def json():
+        pass
 
+class TextOutput(Output):
+    type = 'text-output'
+    def __init__(self, message: str):
+        self.message = message
 
-def display_image(image_str: str, subtitle: str = "") -> None:
-    return {
-        'message': image_str,
-        'type': 'image-output',
-        "subtitle": subtitle
-    }
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.message,
+        }
 
+class ImageOutput(Output):
+    type = 'image-output'
+    def __init__(self, image_str: str, subtitle: str = ""):
+        self.image_str = image_str
+        self.subtitle = subtitle
 
-def display_link(link_url: str, link_text: str = "Click here") -> None:
-    return {
-        'message': link_url,
-        'type': 'link-output',
-        "linkText": link_text
-    }
+    def json(self):
+        return {
+            'type': self.type,
+            'image_str': self.image_str,
+            'subtitle': self.subtitle,
+        }
 
+class LinkOutput(Output):
+    type = 'link-output'
+    def __init__(self, link_url: str, link_text: str = "Click here"):
+        self.link_url = link_url
+        self.link_text = link_text
 
-def display_file(file, download_text: str = "Download here") -> None:
-    return {
-        'message': file if isinstance(file, str) else upload_file(file),
-        'type': 'file-output',
-        "downloadText": download_text
-    }
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.link_url,
+            'linkText': self.link_text,
+        }
 
-def display_html(html, download_text: str = "Download here") -> None:
-    return {
-        'message': html,
-        'type': 'html-output',
-    }
+class FileOutput(Output):
+    type = 'file-output'
+    def __init__(self, file, download_text: str = "Download here"):
+        self.file = file
+        self.download_text = download_text
 
-def display(fields: List[Dict], button_text: str = "Next"):
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.file if isinstance(self.file, str) else upload_file(self.file),
+            'downloadText': self.download_text,
+        }
+
+class HTMLOutput(Output):
+    type = 'html-output'
+    def __init__(self, html, download_text: str = "Download here"):
+        self.html = html
+        self.download_text = download_text
+
+    def json(self):
+        return {
+            'type': self.type,
+            'message': self.html,
+            'downloadText': self.download_text,
+        }
+
+def display_text(msg: str, button_text: str = "Next") -> None:
+    return display([TextOutput(msg)], button_text)
+
+def display_image(image_str: str, subtitle: str = "", button_text: str = "Next") -> None:
+    return display([ImageOutput(image_str, subtitle)], button_text)
+
+def display_link(link_url: str, link_text: str = "Click here", button_text: str = "Next") -> None:
+    return display([LinkOutput(link_url, link_text)], button_text)
+
+def display_file(file, download_text: str = "Download here", button_text: str = "Next") -> None:
+    return display([FileOutput(file, download_text)], button_text)
+
+def display_html(html, download_text: str = "Download here", button_text: str = "Next") -> None:
+    return display([HTMLOutput(html, download_text)], button_text)
+
+def display(outputs: List[Output], button_text: str = "Next"):
     send({
         'type': 'outputs',
-        'fields': fields,
+        'fields': [output.json() for output in outputs],
         'buttonText': button_text
     })
     receive()
