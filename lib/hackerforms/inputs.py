518ca5de055c89c1fa0a6eab9a64451fe32f729a
Igor Torres <igorctoliveira@gmail.com>
2022-04-20 15:12:12 -0300

    Make all functions work only internally

--- lib/hackerforms/inputs.py
+++ lib/hackerforms/inputs.py
@@ -4,48 +4,44 @@ from .type_classes import FileResponse
 from .socket import send, receive
 
 
-def read(msg: str, button_text: str = "Next") -> str:
-    return read_form([{
+def read_text(msg: str) -> Dict:
+    return {
         'message': msg,
         'type': 'text-input',
-        'buttonText': button_text,
-    }], button_text)
+    }
 
 
-def read_date(msg: str, button_text: str = "Next") -> str:
-    return read_form([{
+def read_date(msg: str) -> Dict:
+    return {
         'message': msg,
         'type': 'date-input',
-        'buttonText': button_text,
-    }], button_text)
+    }
 
 
-def read_file(msg: str, button_text: str = "Next") -> FileResponse:
-    return read_form([{
+def read_file(msg: str) -> Dict:
+    return {
         'message': msg,
         'type': 'file-input',
-        'buttonText': button_text
-    }], button_text)
+    }
 
 
 def read_multiple_choice(msg: str,
                          options: Union[List[str], List[Dict]],
-                         button_text: str = "Next",
-                         multiple: bool = False) -> str:
-    return read_form([{
+                         multiple: bool = False) -> Dict:
+    return {
         'message': msg,
         'type': 'multiple-choice-input',
         'options': options,
         'multiple': multiple
-    }], button_text)
+    }
 
 
-def read_dropdown(name: str, options: Union[List[str], List[Dict]], button_text: str = "Next") -> str:
-    return read_form([{
+def read_dropdown(name: str, options: Union[List[str], List[Dict]]) -> Dict:
+    return {
         'message': name,
         'type': 'dropdown-input',
         'options': options
-    }], button_text)
+    }
 
 def read_form(fields: List[Dict], button_text: str = "Next"):
     send({
--- lib/hackerforms/outputs.py
+++ lib/hackerforms/outputs.py
@@ -3,48 +3,43 @@ from .apis import upload_file
 from .socket import send, receive
 
 
-def display(msg: str, button_text: str = "Next") -> None:
-    return display_group([{
+def display_text(msg: str) -> None:
+    return {
         'message': msg,
         'type': 'text-output',
-        'buttonText': button_text
-    }])
+    }
 
 
-def display_image(image_str: str, button_text: str = "Next", subtitle: str = "") -> None:
-    return display_group([{
+def display_image(image_str: str, subtitle: str = "") -> None:
+    return {
         'message': image_str,
         'type': 'image-output',
-        'buttonText': button_text,
         "subtitle": subtitle
-    }])
+    }
 
 
-def display_link(link_url: str, button_text: str = "Next", link_text: str = "Click here") -> None:
-    return display_group([{
+def display_link(link_url: str, link_text: str = "Click here") -> None:
+    return {
         'message': link_url,
         'type': 'link-output',
-        'buttonText': button_text,
         "linkText": link_text
-    }])
+    }
 
 
-def display_file(file, button_text: str = "Next", download_text: str = "Download here") -> None:
-    return display_group([{
+def display_file(file, download_text: str = "Download here") -> None:
+    return {
         'message': file if isinstance(file, str) else upload_file(file),
         'type': 'file-output',
-        'buttonText': button_text,
         "downloadText": download_text
-    }])
+    }
 
-def display_html(html, button_text: str = "Next", download_text: str = "Download here") -> None:
-    return display_group([{
+def display_html(html, download_text: str = "Download here") -> None:
+    return {
         'message': html,
         'type': 'html-output',
-        'buttonText': button_text
-    }])
+    }
 
-def display_group(fields: List[Dict], button_text: str = "Next"):
+def display(fields: List[Dict], button_text: str = "Next"):
     send({
         'type': 'outputs',
         'fields': fields,
