0fafabb6c4b7f554b600817c4ea5b365c2ccc7b4
Igor Torres <igorctoliveira@gmail.com>
2022-04-22 11:53:30 -0300

    Implement Form with builder pattern

--- lib/hackerforms/inputs.py
+++ lib/hackerforms/inputs.py
@@ -4,6 +4,60 @@ from typing import List, Union, Dict
 from .type_classes import FileResponse
 from .socket import send, receive
 
+class Form:
+    def __init__(self, title: str, button_text: str = 'Next'):
+        self.title = title
+        self.button_text = button_text
+        self.inputs: List[Input] = []
+
+    def read_text(self, message: str):
+        self.inputs.append(TextInput(message))
+        return self
+
+    def read_textarea(self, message: str):
+        self.inputs.append(TextareaInput(message))
+        return self
+        
+    def read_number(self, message: str):
+        self.inputs.append(NumberInput(message))
+        return self
+
+    def read_email(self, message: str):
+        self.inputs.append(EmailInput(message))
+        return self
+
+    def read_phone(self, message: str):
+        self.inputs.append(PhoneInput(message))
+        return self
+
+    def read_date(self, message: str):
+        self.inputs.append(DateInput(message))
+        return self
+
+    def read_file(self, message: str):
+        self.inputs.append(FileInput(message))
+        return self
+
+    def read_dropdown(self, name: str, options: Union[List[str], List[Dict]]):
+        self.inputs.append(DropdownInput(name, options))
+        return self
+
+    def read_multiple_choice(self, message: str,
+                            options: Union[List[str], List[Dict]],
+                            multiple: bool = False):
+        self.inputs.append(MultipleChoiceInput(message, options, multiple))
+        return self
+
+    def run(self):
+        send({
+            'type': 'form-input',
+            'fields': [input.json() for input in self.inputs],
+            'buttonText': self.button_text
+        })
+        answers = receive('payload')
+
+        return [input.convertAnswer(answers[index]) for index, input in enumerate(self.inputs)]
+
 class Input(ABC):
     type: str
 
@@ -54,7 +108,7 @@ class MultipleChoiceInput(Input):
     type = 'multiple-choice-input'
     def __init__(self, message: str, options: Union[List[str], List[Dict]], multiple: bool = False):
         self.message = message
-        self.option = options
+        self.options = options
         self.multiple = multiple
 
     def json(self):
@@ -122,52 +176,3 @@ class PhoneInput(Input):
             'type': self.type,
             'message': self.message,
         }
-
-
-def read_text(message: str, button_text: str = "Next") -> Dict:
-    return read_form([TextInput(message)], button_text)
-
-
-def read_date(message: str, button_text: str = "Next") -> Dict:
-    return read_form([DateInput(message)], button_text)
-
-
-def read_file(message: str, button_text: str = "Next") -> Dict:
-    return read_form([FileInput(message)], button_text)
-
-
-def read_multiple_choice(message: str,
-                         options: Union[List[str], List[Dict]],
-                         multiple: bool = False, 
-                         button_text: str = "Next") -> Dict:
-    
-    return read_form([MultipleChoiceInput(message, options, multiple)], button_text)
-
-
-def read_dropdown(name: str, options: Union[List[str], List[Dict]], button_text: str = "Next") -> Dict:
-    return read_form([DropdownInput(name, options)])
-
-def read_textarea(message: str, button_text: str = "Next") -> str:
-    return read_form([TextareaInput(message)], button_text)
-    
-def read_number(message: str, button_text: str = "Next") -> str:
-    return read_form([NumberInput(message)], button_text)
-
-def read_email(message: str, button_text: str = "Next") -> str:
-    return read_form([EmailInput(message)], button_text)
-
-def read_phone(message: str, button_text: str = "Next") -> dict:
-    return read_form([PhoneInput(message)], button_text)
-
-def read_form(inputs: List[Input], button_text: str = "Next"):
-    send({
-        'type': 'form-input',
-        'fields': [input.json() for input in inputs],
-        'buttonText': button_text
-    })
-    answers = receive('payload')
-
-    if len(inputs) == 1:
-        return inputs[0].convertAnswer(answers[0])
-
-    return [input.convertAnswer(answers[index]) for index, input in enumerate(inputs)]
