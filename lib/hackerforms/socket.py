52bb5827e9ea73c2b70691bcfd3f4fd94181eb04
bernardowerneck <bernardo.s.werneck@gmail.com>
2022-04-26 14:59:10 -0300

    wip

--- lib/hackerforms/socket.py
+++ lib/hackerforms/socket.py
@@ -2,6 +2,7 @@ import os
 import atexit
 from websocket import create_connection
 from .utils import serialize, deserialize
+import webbrowser
 
 
 session_id = os.environ.get('SESSION_ID')
@@ -11,9 +12,14 @@ ws_host = os.environ.get('WS_HOST', 'wss://hackerforms-broker.abstra.cloud')
 ws = create_connection(
     f'{ws_host}/lib?sessionId={session_id}')
 
+if session_id == None:
+    session_id = ws.recv()
+    webbrowser.open(f'localhost:8001/play?sessionId={session_id}')
+
+
 start = ws.recv()
-if start != 'start':
-    raise Exception('Broker not started')
+while start != 'start':
+    start = ws.recv()
 
 def send(data):
     ws.send(serialize(data))
