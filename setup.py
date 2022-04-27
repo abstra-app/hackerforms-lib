fbbbae3fe1035fb54221caf711a07ab07576c4e7
felipereyel <felipe.reyel@gmail.com>
2022-04-27 15:10:24 -0300

    unnest lib

---
+++ setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='hackerforms',
+    version='0.1.0',
+    description='Hacker Forms',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abstra-app/hackerforms-lib",
+    license='MIT',
+    classifiers=['Hacker-forms Lib'],
+    packages=['hackerforms'],
+    install_requires=['setuptools', 'requests', 'websocket-client']
+)
