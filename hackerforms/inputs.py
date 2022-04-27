c03f128defb2976baebaca122eb3cfdb3115c816
bernardowerneck <bernardo.s.werneck@gmail.com>
2022-04-27 15:47:05 -0300

    add docstrings

--- hackerforms/inputs.py
+++ hackerforms/inputs.py
@@ -2,28 +2,112 @@ from typing import List, Union, Dict
 from .form import *
 
 def read(message: str, button_text: str = 'Next'):
+    '''Read a text value from the user simple text input
+
+    Args:
+        message (str): The message to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        str: The value entered by the user
+    '''
     return Form(button_text).read(message).run()
 
 def read_textarea(message: str, button_text: str = 'Next'):
+    '''Read a text value from the user with a text area input
+
+    Args:
+        message (str): The message to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        str: The value entered by the user
+    '''
     return Form(button_text).read_textarea(message).run()
 
 def read_number(message: str, button_text: str = 'Next'):
+    '''Read a number value from the user
+
+    Args:
+        message (str): The message to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        int: The value entered by the user
+    '''
     return Form(button_text).read_number(message).run()
 
 def read_email(message: str, button_text: str = 'Next'):
+    '''Read an email value from the user
+
+    Args:
+        message (str): The message to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        str: The value entered by the user
+    '''
     return Form(button_text).read_email(message).run()
 
 def read_phone(message: str, button_text: str = 'Next'):
+    '''Read a phone number value from the user
+
+    Args:
+        message (str): The message to display to the user  
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        str: The value entered by the user
+    '''
     return Form(button_text).read_phone(message).run()
 
 def read_date(message: str, button_text: str = 'Next'):
+    '''Read a date value from the user
+
+    Args:
+        message (str): The message to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        str: The value entered by the user
+    '''
     return Form(button_text).read_date(message).run()
 
 def read_file(message: str, button_text: str = 'Next'):
+    '''Read a file value from the user
+
+    Args:
+        message (str): The message to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        { "file": File, "url": string, "content": bytes }: The file uploaded by the user the user
+    '''
     return Form(button_text).read_file(message).run()
 
 def read_dropdown(message: str, options: Union[List[str], List[Dict]], button_text: str = 'Next'):
+    '''Read a dropdown value from the user
+
+    Args:
+        message (str): The message to display to the user
+        options (list): The options to display to the user
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        str: The value selected by the user
+    '''
     return Form(button_text).read_dropdown(message, options).run()
 
-def read_multiple_choice(message: str, options: Union[List[str], List[Dict]], multiple: bool = False, button_text: str = 'Next'):
+def read_multiple_choice(message: str, options: Union[List[str], List[Dict]], multiple: bool = False, button_text: str = 'Next'): 
+    '''Read a multiple choice value from the user
+
+    Args:
+        message (str): The message to display to the user
+        options (list): The options to display to the user
+        multiple (bool): Whether the user can select multiple options
+        button_text (str): The text to display on the button that will submit the value
+
+    Returns:
+        list, any: The values/value selected by the user
+    '''
     return Form(button_text).read_multiple_choice(message, options, multiple).run()
--- hackerforms/outputs.py
+++ hackerforms/outputs.py
@@ -2,28 +2,73 @@ from .form import *
 
 
 def display(msg: str, button_text: str = 'Next'):
+    '''Display a message to the user
+
+    Args:
+        msg (str): The message to display to the user
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display(msg).run()
 
 
 def display_image(image_str: str, subtitle: str = "", button_text: str = 'Next'):
+    '''Display an image to the user
+
+    Args:
+        image_str (str): The url of the image to display to the user
+        subtitle (str): The subtitle of the image
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display_image(image_str, subtitle).run()
 
 
 def display_link(link_url: str, link_text: str = "Click here", button_text: str = 'Next'):
+    '''Display a link to the user
+    
+    Args:
+        link_url (str): The url of the link to display to the user
+        link_text (str): The text to display on the link
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display_link(link_url, link_text).run()
 
 
 def display_file(file, download_text: str = "Download here", button_text: str = 'Next'):
+    '''Display a button for the user to download a file
+
+    Args:
+        file (File): The file to download
+        download_text (str): The text to display on the button that will download the file
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display_file(file, download_text).run()
 
 
 def display_html(html: str, button_text: str = 'Next'):
+    '''Display a html snippet to the user
+
+    Args:
+        html (str): The html snippet to display to the user
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display_html(html).run()
 
 
 def display_pandas(df, button_text: str = 'Next'):
+    '''Display a pandas dataframe to the user
+
+    Args:
+        df (DataFrame): The dataframe to display to the user
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display_pandas(df).run()
 
 
 def display_plotly(fig, button_text: str = 'Next'):
+    '''Display a plotly figure to the user
+
+    Args:
+        fig (Figure): The figure to display to the user
+        button_text (str): The text to display on the button that will continue the form
+    '''
     return Form(button_text).display_plotly(fig).run()
