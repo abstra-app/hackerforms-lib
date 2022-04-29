9b752fac7d25510cf45e83cc584071990d252830
felipereyel <felipe.reyel@gmail.com>
2022-04-29 10:32:48 -0300

    bump minor

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='hackerforms',
-    version='0.1.0',
+    version='0.2.0',
     description='Hacker Forms',
     long_description=README,
     long_description_content_type="text/markdown",
