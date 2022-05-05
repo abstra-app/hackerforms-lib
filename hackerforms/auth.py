78b58b244767a9ba9d73e9e14cc363f0e77f0ae5
bernardowerneck <32196628+bernardowerneck@users.noreply.github.com>
2022-05-05 19:30:12 -0300

    Update hackerforms/auth.py
    Co-authored-by: Igor Cesar <igorctoliveira@gmail.com>

--- hackerforms/auth.py
+++ hackerforms/auth.py
@@ -3,8 +3,8 @@ from .socket import send, receive
 def authenticate():
   """Get the users email and verify it
 
-  This method will ask the user to enter their email address and will
-  verify it. It will then return the email address and wether the user
+  This method will request users to fill in their email addresses and 
+  verify them. It will then return the email address and wether the user
   is authenticated or not.
 
   Returns:
