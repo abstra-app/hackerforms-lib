5b47e6241b3a55afce1c3da3e1cd337e11bc112a
bernardowerneck <bernardo.s.werneck@gmail.com>
2022-05-05 19:05:40 -0300

    add docstring

--- hackerforms/auth.py
+++ hackerforms/auth.py
@@ -1,6 +1,15 @@
 from .socket import send, receive
 
 def authenticate():
+  """Get the users email and verify it
+
+  This method will ask the user to enter their email address and will
+  verify it. It will then return the email address and wether the user
+  is authenticated or not.
+
+  Returns:
+    { 'valid': Boolean, 'info': { 'email': str } }
+  """
   send({"type": "auth:initialize"})
   while True:
     auth = receive()
