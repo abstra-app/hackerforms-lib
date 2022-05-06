e05e506500a49c2bf8bddffdc66e3a1479650312
bernardowerneck <32196628+bernardowerneck@users.noreply.github.com>
2022-05-06 18:48:45 -0300

    create auth response class (#24)

--- hackerforms/auth.py
+++ hackerforms/auth.py
@@ -4,14 +4,22 @@ def authenticate():
   """Get the users email and verify it
 
   This method will request users to fill in their email addresses and 
-  verify them. It will then return the email address and wether the user
-  is authenticated or not.
+  verify them. It will then return the email address once it has been validated.
 
   Returns:
-    { 'valid': Boolean, 'info': { 'email': str } }
+    AuthReponse
   """
   send({"type": "auth:initialize"})
   while True:
     auth = receive()
     if auth["type"] == "auth:validation-ended":
-      return auth
+      return AuthResponse(auth["email"])
+
+class AuthResponse:
+  """The response from the authentication process
+  
+  Attributes:
+    email (str): The email address of the user
+  """
+  def __init__(self, email: str):
+    self.email = email
