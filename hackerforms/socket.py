eb768ae322efb9257fa5bd4aa85710b63d6ebd89
bernardowerneck <bernardo.s.werneck@gmail.com>
2022-05-13 09:26:05 -0300

    preparing for test

--- hackerforms/socket.py
+++ hackerforms/socket.py
@@ -6,21 +6,42 @@ from websocket import create_connection
 from .exit_hook import hooks
 from .utils import serialize, deserialize
 from .parameters import set_params
+import os
+
+initialized = False
+def initialize():
+    global ws, initialized
+    initialized = True
+    session_id = os.environ.get('SESSION_ID')
+    # ws_host = os.environ.get('WS_HOST', 'ws://localhost:8080')
+    ws_host = os.environ.get('WS_HOST', 'wss://hackerforms-broker.abstra.cloud')
+
+    # frontend_host = os.environ.get('FRONTEND_HOST', 'http://localhost:8001')
+    frontend_host = os.environ.get('FRONTEND_HOST', 'https://hackerforms.com')
+
+    if session_id == None:
+        ws = create_connection(f'{ws_host}/lib')
+        session_id = receive('sessionId')
+        webbrowser.open(f'{frontend_host}/local/{session_id}')
+    else:
+        ws = create_connection(f'{ws_host}/lib?sessionId={session_id}')
 
 
-session_id = os.environ.get('SESSION_ID')
-# ws_host = os.environ.get('WS_HOST', 'ws://localhost:8080')
-ws_host = os.environ.get('WS_HOST', 'wss://hackerforms-broker.abstra.cloud')
+    start = { "type": None }
+    while start["type"] != 'start':
+        start = receive()
 
-# frontend_host = os.environ.get('FRONTEND_HOST', 'http://localhost:8001')
-frontend_host = os.environ.get('FRONTEND_HOST', 'https://hackerforms.com')
+    set_params(start["params"])
+    
 
 
 def send(data):
+    if not initialized: return
     ws.send(serialize(data))
 
 
 def receive(path: str = ''):
+    if not initialized: return
     data = deserialize(ws.recv())
 
     if data['type'] == 'keep-alive':
@@ -31,22 +52,9 @@ def receive(path: str = ''):
     return data.get(path, None)
 
 
-if session_id == None:
-    ws = create_connection(f'{ws_host}/lib')
-    session_id = receive('sessionId')
-    webbrowser.open(f'{frontend_host}/local/{session_id}')
-else:
-    ws = create_connection(f'{ws_host}/lib?sessionId={session_id}')
-
-
-start = { "type": None }
-while start["type"] != 'start':
-    start = receive()
-
-set_params(start["params"])
-
 @atexit.register
 def close():
+    if not initialized: return
     send({
         'type': 'program:end',
         'exitCode': hooks.exit_code,
