4cd22ca7df702246f7d463d1e0cbc9b4e6622185
Pedro Reis <pedroreisv@gmail.com>
2022-05-17 15:58:37 -0300

    Revert "Revert "Protocol validation (#27)""
    This reverts commit 846dbf8cffd6f47fd1363e5af963388fd26c291a.

--- hackerforms/__init__.py
+++ hackerforms/__init__.py
@@ -1,6 +1,13 @@
 from .page import *
 from .inputs import *
 from .outputs import *
-from .stdio import *
+from .stdio import initialize as _initialize_stdio
 from .auth import *
 from .parameters import url_params
+from .socket import initialize as _initialize_socket
+import os
+
+
+if os.environ["ENV"] != "test":
+  _initialize_socket()
+  _initialize_stdio()
