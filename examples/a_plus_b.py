3f4cb569bfd942621ccde3a33e74077658407e7f
felipereyel <felipe.reyel@gmail.com>
2022-05-30 14:07:12 -0300

    translante to eng

--- examples/a_plus_b.py
+++ examples/a_plus_b.py
@@ -2,8 +2,8 @@ from hackerforms import read, display
 
 print("aqui tem print!")
 
-a = read("Qual o valor de a?")
-b = read("Qual o valor de b?")
+a = read("What is the value of a?")
+b = read("What is the value of b?")
 soma = a + b
 
 display("a + b = " + str(soma))
