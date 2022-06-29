5f5948e848b638919a40c3712573452a0177b1bd
Igor Cesar <igorctoliveira@gmail.com>
2022-06-29 15:51:43 -0300

    Sync files from protocol (#58)
    * Sync files from protocol
    
    * Update widgets
    
    * Move tests to generated folder
    
    Co-authored-by: Abstra Bot <dev@abstra.app>

--- hackerforms/generated/input_types.py
+++ hackerforms/generated/input_types.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod, ABC
-from typing import List, Union, Dict
+from typing import List, Union, Dict, Optional, Any
 from datetime import date
+import datetime
 import json
 
-from .type_classes import FileResponse, PhoneResponse
-
+from .response_types import FileResponse, PhoneResponse
 
 class Input(ABC):
     type: str
@@ -14,17 +14,29 @@ class Input(ABC):
         self.key = key
 
     @abstractmethod
-    def json():
+    def json(self):
         pass
 
+    @abstractmethod
     def convert_answer(self, answer):
-        return answer
-
+        pass
 
 class TextInput(Input):
     type = 'text-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a text value from the user simple text input
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            placeholder (str): The placeholder text to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', '')
@@ -47,6 +59,14 @@ class TextInput(Input):
             'fullWidth': self.full_width
         }
 
+    def convert_answer(self, answer: str) -> str:
+        '''
+        Returns:
+            str: The value entered by the user
+        '''
+        return answer
+
+
 
 class ExecuteJs(Input):
     type = 'execute-js'
@@ -65,11 +85,30 @@ class ExecuteJs(Input):
             'key': self.key,
         }
 
+    def convert_answer(self, answer: str) -> str:
+        '''
+        Returns:
+            string: Serialized return value of the executed JavaScript
+        '''
+        return answer
+
+
 
 class TagInput(Input):
+
     type = 'tag-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a tag value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str or float): The initial value to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', [])
@@ -92,11 +131,31 @@ class TagInput(Input):
             'fullWidth': self.full_width
         }
 
+    def convert_answer(self, answer: List[Union[str,float]]) -> List[Union[str,float]]:
+        '''
+        Returns:
+            List[Union[str,float]]: The value entered by the user
+        '''
+        
+        return answer
+
+
 
 class DateInput(Input):
     type = 'date-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a date value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', None)
@@ -117,11 +176,10 @@ class DateInput(Input):
             'fullWidth': self.full_width
         }
 
-    def convert_answer(self, answer: str):
-        '''Convert answer from string to date
-
-        Args:
-            answer (str): Date format YYYY-MM-DD
+    def convert_answer(self, answer: str) -> Optional[datetime.date]:
+        '''
+        Returns:
+            datetime.date: The value entered by the user
         '''
         if not answer:
             return None
@@ -137,6 +195,17 @@ class FileInput(Input):
     type = 'file-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a file value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', '')
@@ -159,7 +228,11 @@ class FileInput(Input):
             'fullWidth': self.full_width
         }
 
-    def convert_answer(self, answer):
+    def convert_answer(self, answer) -> Optional[FileResponse]:
+        '''
+        Returns:
+            FileResponse: The file uploaded by the user the user
+        '''
         if not answer:
             return None
 
@@ -173,6 +246,16 @@ class ImageInput(Input):
     type = 'image-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a image file value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', '')
@@ -192,11 +275,14 @@ class ImageInput(Input):
             'columns': self.columns,
             'required': self.required,
             'multiple': self.multiple,
-            'required': self.required,
             'fullWidth': self.full_width,
         }
 
-    def convert_answer(self, answer):
+    def convert_answer(self, answer) -> Optional[FileResponse]:
+        '''
+        Returns:
+            FileResponse: The image file uploaded by the user the user
+        '''
         if not answer:
             return None
 
@@ -210,6 +296,18 @@ class MultipleChoiceInput(Input):
     type = 'multiple-choice-input'
 
     def __init__(self, key: str, message: str, options: Union[List[str], List[Dict]], **kwargs):
+        '''Read a multiple choice value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+            options (list): The options to display to the user, eg. ['Option 1', 'Option 2'] or [{'label': 'Option 1', 'value': '1'}, {'label': 'Option 2', 'value': '2'}]
+
+        Keyword Arg(s):
+            multiple (bool): Whether the user can select multiple options
+            button_text (str): The text to display on the button that will submit the value
+            initial_value: The initial value to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.message = message
         self.options = options
@@ -234,11 +332,33 @@ class MultipleChoiceInput(Input):
             'fullWidth': self.full_width,
         }
 
+    def convert_answer(self, answer: Union[List, Any]) -> Union[List, Any]:
+        '''
+        Returns:
+            list, any: The values/value selected by the user
+        '''
+        return answer
+
+
 
 class CardsInput(Input):
     type = 'cards-input'
 
-    def __init__(self, key: str, label: str, options, **kwargs) -> None:
+    def __init__(self, key: str, label: str, options, **kwargs):
+        '''Read cards from the user
+
+        Positional Arg(s):
+            label (str): The text related to this field
+            options (list): The options to display to the user, eg. [
+                                {'title': 'Option 1', 'image': 'https://image_1.png', 'description': 'option 1 description'}, 
+                                {'title': 'Option 2', 'image': 'https://image_2.png', 'description': 'option 2 description'}]
+
+        Keyword Arg(s):
+            multiple (bool): Whether the user can select multiple options
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (list): The initial value to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.label = label
         self.options = options
@@ -265,13 +385,34 @@ class CardsInput(Input):
             'fullWidth': self.full_width,
         }
 
+    def convert_answer(self, answer: Union[List, Any]) -> Union[List, Any]:
+        '''
+        Returns:
+            list, any: The options/option selected by the user
+        '''
+        return answer
+
+
 
 class DropdownInput(Input):
     type = 'dropdown-input'
 
-    def __init__(self, key: str, name: str, options: Union[List[str], List[Dict]], **kwargs):
+    def __init__(self, key: str, message: str, options: Union[List[str], List[Dict]], **kwargs):
+        '''Read a dropdown value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+            options (list): The options to display to the user, eg. ['Option 1', 'Option 2'] or [{'label': 'Option 1', 'value': '1'}, {'label': 'Option 2', 'value': '2'}]
+
+        Keyword Arg(s):
+            multiple (bool): Whether the user can select multiple options
+            button_text (str): The text to display on the button that will submit the value
+            initial_value: The initial value to display to the user
+            placeholder (str): The placeholder text to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
-        self.name = name
+        self.message = message
         self.options = options
         self.initial_value = kwargs.get('initial_value', None)
         self.required = kwargs.get('required', True)
@@ -285,7 +426,7 @@ class DropdownInput(Input):
         return {
             'type': self.type,
             'key': self.key,
-            'message': self.name,
+            'message': self.message,
             'options': self.options,
             'hint': self.hint,
             'multiple': self.multiple,
@@ -296,18 +437,38 @@ class DropdownInput(Input):
             'fullWidth': self.full_width,
         }
 
+    def convert_answer(self, answer: str) -> str:
+        '''
+        Returns:
+            str: The value selected by the user
+        '''
+        return answer
+
+
 
 class TextareaInput(Input):
     type = 'textarea-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a text value from the user with a text area input
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            placeholder (str): The placeholder text to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', '')
         self.required = kwargs.get('required', True)
-        self.hint = kwargs.get('hint', None)
         self.placeholder = kwargs.get('placeholder', 'Your answer here')
         self.columns = kwargs.get('columns', 1)
+        self.hint = kwargs.get('hint', None)
         self.full_width = kwargs.get('full_width', False)
 
     def json(self):
@@ -318,16 +479,36 @@ class TextareaInput(Input):
             'initialValue': self.initial_value,
             'placeholder': self.placeholder,
             'required': self.required,
-            'hint': self.hint,
             'columns': self.columns,
+            'hint': self.hint,
             'fullWidth': self.full_width,
         }
+
+    def convert_answer(self, answer: str) -> str:
+        '''
+        Returns:
+            str: The value entered by the user
+        '''
+        return answer
+
 
 
 class NumberInput(Input):
     type = 'number-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a number value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            message (str): The message to display to the user
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            placeholder (str): The placeholder text to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', 0)
@@ -350,11 +531,30 @@ class NumberInput(Input):
             'fullWidth': self.full_width,
         }
 
+    def convert_answer(self, answer: int) -> int:
+        '''
+        Returns:
+            int: The value entered by the user
+        '''
+        return answer
+
+
 
 class EmailInput(Input):
     type = 'email-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read an email value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            placeholder (str): The placeholder text to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', '')
@@ -377,11 +577,30 @@ class EmailInput(Input):
             'fullWidth': self.full_width,
         }
 
+    def convert_answer(self, answer: str) -> str:
+        '''
+        Returns:
+            str: The value entered by the user
+        '''
+        return answer
+
+
 
 class PhoneInput(Input):
     type = 'phone-input'
 
     def __init__(self, key: str, message: str, **kwargs):
+        '''Read a phone number value from the user
+
+        Positional Arg(s):
+            message (str): The message to display to the user
+
+        Keyword Arg(s):  
+            button_text (str): The text to display on the button that will submit the value
+            initial_value (str): The initial value to display to the user
+            placeholder (str): The placeholder text to display to the user
+            required (bool or str): Whether the input is required or not eg. "this field is required"
+        '''
         super().__init__(key)
         self.message = message
         self.initial_value = kwargs.get('initial_value', '')
@@ -404,14 +623,26 @@ class PhoneInput(Input):
             'fullWidth': self.full_width,
         }
 
-    def convert_answer(self, answer):
+    def convert_answer(self, answer) -> Optional[PhoneResponse]:
+        '''
+        Returns:
+            PhoneResponse: The value entered by the user
+        '''
         return PhoneResponse(raw=answer['raw'], masked=answer['masked']) if answer else None
 
 
 class ListInput(Input):
     type = 'list-input'
 
-    def __init__(self, key: str, item_schema, **kwargs):
+    def __init__(self, key: str, item_schema: Any, **kwargs):
+        '''Read a list value from the user
+
+        Positional Arg(s):
+            item_schema (ListItemSchema): The schema for the items of the list
+
+        Keyword Arg(s):
+            button_text (str): The text to display on the button that will submit the value
+        '''
         super().__init__(key)
         self.item_schema = item_schema
         self.initial_value = kwargs.get('initial_value', [{}])
@@ -436,14 +667,28 @@ class ListInput(Input):
             'fullWidth': self.full_width,
         }
 
-    def convert_answer(self, answers):
+    def convert_answer(self, answers) -> List:
+        '''
+        Returns:
+            list: The values entered by the user
+        '''
         return [self.item_schema.convert_answer(answer) for answer in answers]
 
-
 class PandasRowSelectionInput(Input):
+
     type = 'pandas-row-selection-input'
 
-    def __init__(self, key: str, df, **kwargs):
+    def __init__(self, key: str, df: Any, **kwargs):
+        """Display a pandas dataframe as a table and allow the user to select rows
+
+            Positional Arg(s):
+                df (pandas.DataFrame): The pandas dataframe to be displayed
+
+            Keyword Arg(s):
+                required: Whether the input is required or not
+                button_text (string): The text to display on the next step button
+            
+        """
         super().__init__(key)
         self.df = df
         self.required = kwargs.get('required', True)
@@ -461,3 +706,10 @@ class PandasRowSelectionInput(Input):
             'columns': self.columns,
             'fullWidth': self.full_width,
         }
+
+    def convert_answer(self, answer) -> List:
+        '''
+        Returns:
+            The list of selected rows
+        '''
+        return answer
--- hackerforms/generated/tests/test_input_types.py
+++ hackerforms/generated/tests/test_input_types.py
@@ -4,9 +4,9 @@ from .example_instances import example_instances
 
 METADATA_FILEPATH = os.path.join(os.path.dirname(__file__), 'metadata.json')
 
-
 def test_function():
     widgets_metadata = load_metadata()["widgets"]
+    del widgets_metadata["list-input"]
     generated_widgets = load_widget_test_instances()
     assert_widget_examples_contains_all_widgets(
         generated_widgets, widgets_metadata)
@@ -14,7 +14,6 @@ def test_function():
         assert_widget_exists_in_metadata(widget_example, widgets_metadata)
         assert_widgets_props_are_right(
             widget_example, widgets_metadata[widget_example["type"]])
-
 
 def load_metadata():
     with open(METADATA_FILEPATH) as f:
@@ -29,14 +28,17 @@ def assert_widget_exists_in_metadata(widget_example, widgets_metadata):
     assert widget_example["type"] in widgets_metadata
 
 
-def assert_widgets_props_are_right(widget_example, widget_metadata):
-    for prop in widget_metadata:
+def assert_widgets_props_are_right(widget_example, metadata_widget):
+    for prop in metadata_widget['params']:
         assert prop in widget_example
-        assert types_compatible(widget_example[prop], widget_metadata[prop])
+        assert types_compatible(widget_example[prop], metadata_widget['params'][prop])
+    for prop in metadata_widget['optionals']:
+        assert prop in widget_example
+        assert types_compatible(widget_example[prop], metadata_widget['optionals'][prop])
     for prop in widget_example:
         if prop == "type":
             continue
-        assert prop in widget_metadata
+        assert prop in metadata_widget['params'] or prop in metadata_widget['optionals']
 
 
 def types_compatible(prop, schema):
@@ -78,4 +80,5 @@ def to_generic_type(type):
 
 def assert_widget_examples_contains_all_widgets(generated_widgets, widgets_metadata):
     for widget_type in widgets_metadata:
-        assert any(widget["type"] == widget_type for widget in generated_widgets)
+        if widget_type != "list-input":
+            assert any(widget["type"] == widget_type for widget in generated_widgets if widget["type"] != "list-input")
