437a4631faafe651b34ffe2e6c6b335bed76f92b
Lucas Machado <lucasmachado.rj@gmail.com>
2022-07-21 14:42:27 -0300

    overwrite python builtin input and raw_input (#62)
    * overwrite python builtin input and raw_input

--- hackerforms/__init__.py
+++ hackerforms/__init__.py
@@ -6,9 +6,11 @@ from .stdio import initialize as _initialize_stdio
 from .auth import *
 from .parameters import url_params
 from .socket import initialize as _initialize_socket
+from .input import overload_input as _overload_input
 import os
 
 
 if os.environ.get("ENV") != "test":
     _initialize_socket()
     _initialize_stdio()
+    _overload_input()
