aaa66104916019986487058cd36ba05645a039fc
felipereyel <felipe.reyel@gmail.com>
2022-08-01 16:14:42 -0300

    version on env TAG

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+import os
+import re
 import pathlib
 from setuptools import setup
+
+regex = "^v(\d+\.\d+\.\d+)$"
+TAG = os.getenv("TAG")
+if not TAG or not re.search(regex, TAG):
+    raise ValueError("TAG enviroment variable must be in the format v1.2.3")
+version = re.search(regex, TAG).group(1)
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -8,14 +16,13 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name='hackerforms',
-    version='0.7.0',
-    description='Hacker Forms',
+    name="hackerforms",
+    version=version,
+    description="Hacker Forms",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/abstra-app/hackerforms-lib",
-    license='MIT',
-    packages=['hackerforms', 'hackerforms.generated'],
-    install_requires=['setuptools', 'requests',
-                      'websocket-client', 'validators']
+    license="MIT",
+    packages=["hackerforms", "hackerforms.generated"],
+    install_requires=["setuptools", "requests", "websocket-client", "validators"],
 )
