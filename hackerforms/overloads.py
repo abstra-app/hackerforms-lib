25f575aad8857a1db413b4a6a4f965f4aab12fc6
João Henrique Franco <joaohenrique.franco@gmail.com>
2022-08-30 17:22:26 -0300

    Add print() as display() override (#67)
    * Add function and call it
    
    * Add missing import
    
    * Fix import
    
    * Fix envvar use

--- hackerforms/overloads.py
+++ hackerforms/overloads.py
@@ -1,7 +1,12 @@
 import builtins
 from .generated.inputs import read
+from .generated.outputs import display
 
 
 def overload_input():
     builtins.input = read
     builtins.raw_input = read
+
+
+def overload_print():
+    builtins.print = display
