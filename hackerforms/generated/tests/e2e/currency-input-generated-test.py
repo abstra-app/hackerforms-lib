c89f587dc693e423b38490f94e1711c7bd7f8f5f
Lucas Machado <lucasmachado.rj@gmail.com>
2022-11-17 14:03:52 -0300

    test jenkins trigger

--- hackerforms/generated/tests/e2e/currency-input-generated-test.py
+++ hackerforms/generated/tests/e2e/currency-input-generated-test.py
@@ -4,7 +4,7 @@
 ###############################################################################
 from hackerforms import *
 
-
+# tests
 output = read_currency("text to be displayed", currency="BRL", full_width=True)
 assert output == 23.3
 
