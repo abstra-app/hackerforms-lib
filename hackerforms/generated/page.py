4d76e1d88d8f9e5f2b825d0d9f5dbc943ed28ae4
bernardowerneck <bernardo.s.werneck@gmail.com>
2022-11-21 15:25:53 -0300

    remove duplicated

--- hackerforms/generated/page.py
+++ hackerforms/generated/page.py
@@ -484,6 +484,30 @@ class WidgetSchema:
         self.widgets.append(PhoneInput(key, label, **kwargs))
         return self
 
+    def read_rating(self, label: str, **kwargs):
+        """Read a number value from the user using a slider
+
+        Positional Args:
+          label (str): The label to display to the user
+
+        Keyword Args:
+          initial_value (str): The initial value to display to the user. Defaults to 0.
+          required (bool or str): Whether the input is required or not eg. "this field is required". Defaults to True.
+          hint (str): A tooltip displayed to the user. Defaults to None.
+          full_width (bool): Whether the input should use full screen width. Defaults to False.
+          max (float): Max value accepted by the input. Defaults to None.
+          char (str): Which char should be displayed as icon?
+          columns: The number of columns of the input
+          key: The key of the input's value on the form result. Defaults to the label arg
+
+
+        Returns:
+          The form object
+        """
+        key = kwargs.pop("key", label)
+        self.widgets.append(RatingInput(key, label, **kwargs))
+        return self
+
     def read_tag(self, label: str, **kwargs):
         """Read a tag value from the user
 
