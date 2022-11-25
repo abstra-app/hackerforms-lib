b87d0fc5df12dc7ab819bccb2c8b5fbaa1c6fba5
bernardowerneck <bernardo.s.werneck@gmail.com>
2022-11-25 14:24:13 -0300

    remove execute_js from template

--- hackerforms/automation/parser/templates/read_functions_template.py
+++ hackerforms/automation/parser/templates/read_functions_template.py
@@ -2,20 +2,6 @@ read_functions_template = """
 import typing
 from hackerforms.page import Page
 
-def execute_js(code: str, **kwargs):
-    '''Execute JavaScript on the page
-    Args:
-        code: The JS code to be executed
-    Keyword Arg:
-        context (dict): variables to be passed to the JS code
-        button_text (string): The text to display on the next step button
-    Returns:
-      string: Serialized return value of the executed JavaScript
-    '''
-
-    button_text = kwargs.get('button_text', 'Next')
-    return get_single_value(Page().execute_js(code, **kwargs).run(button_text))
-
 {%- for widget in widgets %}
 {% if widgets[widget]['func_name'] == 'read_text' %}
 def read({{widgets[widget]['signature_params']}}):
