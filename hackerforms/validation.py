5f454579ab416e5dcccb5fea0c4cc04a401fee50
Lucas Machado <lucasmachado.rj@gmail.com>
2022-11-29 16:46:30 -0300

    New protocol widget schema (#40)
    Co-authored-by: Bruno Vieira Costa <bruno@abstra.app>

--- hackerforms/validation.py
+++ hackerforms/validation.py
@@ -2,26 +2,27 @@ from .generated.metadata import metadata
 
 
 def types_compatible(prop, schema):
-    if type(schema["type"]) == list:
+    if type(schema["typeName"]) == list:
         return any(
-            types_compatible(prop, {**schema, "type": type}) for type in schema["type"]
+            types_compatible(prop, {**schema, "typeName": type})
+            for type in schema["typeName"]
         )
 
-    if schema["type"] == "string":
+    if schema["typeName"] == "string":
         return type(prop) == str
-    if schema["type"] == "number":
+    if schema["typeName"] == "number":
         return type(prop) == int or type(prop) == float
-    if schema["type"] == "boolean":
+    if schema["typeName"] == "boolean":
         return type(prop) == bool
-    if schema["type"] == "array":
+    if schema["typeName"] == "array":
         return type(prop) == list
-    if schema["type"] == "object":
+    if schema["typeName"] == "object":
         return type(prop) == dict
-    if schema["type"] == "null":
+    if schema["typeName"] == "null":
         return prop == None
-    if schema["type"] == "any":
+    if schema["typeName"] == "any":
         return True
-    raise Exception(f"Unknown type: {schema['type']}")
+    raise Exception(f"Unknown type: {schema['typeName']}")
 
 
 def valid_prop(prop, schema):
@@ -51,28 +52,45 @@ def to_generic_type(type):
 
 def validate_widget_props(widget):
     metadata_widget = metadata[widget["type"]]
-    for prop in metadata_widget["params"]:
-        if not prop in widget:
-            raise Exception(f"{prop} not in {widget.keys()}")
-        assert types_compatible(widget[prop], metadata_widget["params"][prop])
-        assert valid_prop(widget[prop], metadata_widget["params"][prop])
-    for prop in metadata_widget["optionals"]:
+    required_props = [
+        prop
+        for prop in metadata_widget["brokerAPI"]["params"]
+        if not prop.get("isOptional", False)
+    ]
+
+    for prop in required_props:
+        prop_name = prop["argName"]
+        if not prop_name in widget:
+            raise Exception(f"{prop_name} not in {widget.keys()}")
+        assert types_compatible(widget[prop_name], prop)
+        assert valid_prop(widget[prop_name], prop)
+
+    optional_props = [
+        prop
+        for prop in metadata_widget["brokerAPI"]["params"]
+        if prop.get("isOptional", False)
+    ]
+    for prop in optional_props:
+        prop_name = prop["argName"]
         if not (
-            widget.get(prop) is None
+            widget.get(prop_name) is None
             or (
-                types_compatible(widget[prop], metadata_widget["optionals"][prop])
-                and valid_prop(widget[prop], metadata_widget["optionals"][prop])
+                types_compatible(widget[prop_name], prop)
+                and valid_prop(widget[prop_name], prop)
             )
         ):
+            print(types_compatible(widget[prop_name], prop))
+            print(widget.get(prop_name))
+            print(valid_prop(widget[prop_name], prop))
             raise Exception(
-                f"{widget[prop]}: {type(widget[prop])} is not compatible with {metadata_widget['optionals'][prop]}"
+                f"{prop_name}: {type(widget[prop_name])} is not compatible with {prop}"
             )
     for prop in widget:
         if prop == "type":
             continue
         if not (
-            prop in metadata_widget["params"] or prop in metadata_widget["optionals"]
+            prop in param["argName"] for param in metadata_widget["brokerAPI"]["params"]
         ):
             raise Exception(
-                f'{prop} not in {metadata_widget["params"].keys()} nor {metadata_widget["optionals"].keys()}'
+                f'{prop} not in {metadata_widget["brokerAPI"]["params"].keys()}'
             )
